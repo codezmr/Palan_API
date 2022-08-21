f38f7235a46e80bab34c3b6bf80eb1587eac4318
codezmr <mdzamiruddin.zmr@gmail.com>
2022-08-21 10:08:36 +0530

    Rest Controller Methods implemented

---
+++ 01_PLANS_API/src/main/java/com/codezmr/rest/PlanRestController.java
@@ -0,0 +1,111 @@
+package com.codezmr.rest;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codezmr.entity.Plan;
+import com.codezmr.service.PlanService;
+
+@RestController
+public class PlanRestController {
+
+	@Autowired
+	private PlanService planService;
+
+	@GetMapping("/categories")
+	public ResponseEntity<Map<Integer, String>> planCategories() {
+		Map<Integer, String> categories = planService.getPlanCategories();
+		return new ResponseEntity<>(categories, HttpStatus.OK);
+	}
+
+	@PostMapping("/plan")
+	public ResponseEntity<String> savePlan(@RequestBody Plan plan) {
+
+		String responseMsg = "";
+		boolean isSaved = planService.savePlan(plan);
+
+		if (isSaved) {
+			responseMsg = "Plan Saved";
+		} else {
+			responseMsg = "Plan not saved";
+		}
+
+		return new ResponseEntity<>(responseMsg, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/plans")
+	public ResponseEntity<List<Plan>> plans() {
+
+		List<Plan> allPlans = planService.getAllPlans();
+		return new ResponseEntity<>(allPlans, HttpStatus.OK);
+	}
+
+	@GetMapping("/plan/{planId}")
+	public ResponseEntity<Plan> editPlan(@PathVariable Integer planId) {
+
+		Plan plan = planService.getPlanById(planId);
+		return new ResponseEntity<>(plan, HttpStatus.OK);
+
+	}
+	@PutMapping("/plan")
+	public ResponseEntity<String> updatePlan(Plan plan) {
+		boolean isUpdated = planService.updatePlan(plan);
+
+		String msg = "";
+
+		if (isUpdated) {
+
+			msg = "Plan updated";
+		} else {
+			msg = "Plan Not updated";
+		}
+		return new ResponseEntity<>(msg, HttpStatus.OK);
+
+	}
+
+	@DeleteMapping("/plan/{planId}")
+	public ResponseEntity<String> deletePlan(@PathVariable Integer planId) {
+
+		boolean isDeleted = planService.deletePlanById(planId);
+
+		String msg = "";
+
+		if (isDeleted) {
+
+			msg = "Plan Deleted";
+		} else {
+			msg = "Plan Not Deleted";
+		}
+		return new ResponseEntity<>(msg, HttpStatus.OK);
+
+	}
+
+	@PutMapping("/status-change/{planId}/{status}")
+	public ResponseEntity<String> statusChange( @PathVariable Integer planId, @PathVariable String status) {
+
+		boolean isStatusChanged = planService.planStatusChange(planId, status);
+
+		String msg = "";
+
+		if (isStatusChanged) {
+
+			msg = "Plan Status Changed";
+		} else {
+			msg = "Plan Status Not Changed";
+		}
+		return new ResponseEntity<>(msg, HttpStatus.OK);
+
+	}
+
+}
